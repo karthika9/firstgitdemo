4f3a7589684b8876126d860a033ad4871cb824b0
jeyakarthikkanainarsureshkumar <jeyakarthikkanainarsureshkumar@localhost>
2021-04-13 10:48:28 -0400

    fibonacci

---
+++ firstgitdemo/src/firstgitdemo/Test1.java
@@ -0,0 +1,18 @@
+package firstgitdemo;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		int num1=1;
+		int num2 = 1;
+		int sum = 0;
+		System.out.print(num1 +" "+ num2);
+		for(int i=2;i<=10;i++) {
+			sum= num1 + num2;
+			num1=num2;
+			num2=sum;
+			System.out.print(" " +sum);
+	}
+
+}
+}
